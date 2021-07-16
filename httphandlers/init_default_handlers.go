0f9202df874aa6206caba96bc22fa3c371bd18e4
Zhanat <iskakov_zhanat@mail.ru>
2021-07-16 09:33:32 +0600

    0.1.4

--- httphandlers/init_default_handlers.go
+++ httphandlers/init_default_handlers.go
@@ -6,7 +6,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func InitDefaultHandlers(mux *http.ServeMux) {
+func InitDefaultHandlers(mux http.Handler) {
 	http.Handle("/metrics", promhttp.Handler())
 	http.HandleFunc("/health-check", HealthCheck)
 	http.Handle("/api/v1/", AccessControl(mux))
