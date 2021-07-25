eb6e2cdfaa12956b2b98fe07fe5eefd8ad0ad99b
Zhanat <iskakov_zhanat@mail.ru>
2021-07-25 11:25:07 +0600

    0.3.0

--- gokitmiddlewares/endpoint.go
+++ gokitmiddlewares/endpoint.go
@@ -1,18 +1,19 @@
 package gokitmiddlewares
 
 import (
-	"github.com/Zhanat87/common-libs/tracers"
-	"github.com/go-kit/kit/endpoint"
 	"time"
+
+	"github.com/go-kit/kit/endpoint"
+	"github.com/openzipkin/zipkin-go"
 )
 
-func GetDefaultEndpoint(endPoint endpoint.Endpoint, endPointName string) endpoint.Endpoint {
-	return GetCircuitBreakerEndpoint(
-		GetRateLimiterEndpoint(
-			GetZipkinEndpoint(tracers.ZipkinTracer, endPoint,
-				endPointName), 100), endPointName, 30 * time.Second)
+func GetDefaultEndpoint(endPoint endpoint.Endpoint, endPointName string, zipkinTracer *zipkin.Tracer) endpoint.Endpoint {
+	return GetCircuitBreakerEndpoint(GetRateLimiterEndpoint(GetZipkinEndpoint(
+		zipkinTracer, endPoint, endPointName), time.Second, 100), endPointName, 30*time.Second)
 }
 
-func GetEndpoint(endPoint endpoint.Endpoint, endPointName string, circuitBreakerTimeout time.Duration) endpoint.Endpoint {
-	return GetCircuitBreakerMiddleware(name, timeout)(endPoint)
+func GetEndpoint(endPoint endpoint.Endpoint, endPointName string, zipkinTracer *zipkin.Tracer,
+	limiterInterval time.Duration, limiterBurst int, circuitBreakerTimeout time.Duration) endpoint.Endpoint {
+	return GetCircuitBreakerEndpoint(GetRateLimiterEndpoint(GetZipkinEndpoint(
+		zipkinTracer, endPoint, endPointName), limiterInterval, limiterBurst), endPointName, circuitBreakerTimeout)
 }
